33e529d23bac4a255703c26f112a9e35e51669ed
dabinlee708 <dabinlee708@Dabin-ThinkPad--E440>
2015-07-06 16:56:57 +0800

    Sampling bypass implemented

--- Quantization/Quantization.py
+++ Quantization/Quantization.py
@@ -45,17 +45,22 @@ def Quant2(OpenName1,OpenName2,Alpha=0.2,Num_Samples=64,Filtering=True):
     Quant(OpenName2,Alpha,Num_Samples,Filtering)
 
 def Quant(OpenName,Alpha=0.2,Num_Samples=64,Filtering=True):
+    #Set sampling count
+    if Num_Samples==0:
+        Sampling=False
+    else:
+        Sampling=True
     
     #Open csv file as a list \
     with open(os.getcwd()+"//Source_Data//"+OpenName+".csv",'rb') as csvfile:
         reader = csv.reader(csvfile)
         val_list = list(reader)
         
-    #Remove inapprorpirate values
+    #Remove inapproprirate values
     #print "Number of elements:",len(val_list)
     while val_list.count(['-Infinity']) > 0:
         val_list.remove(['-Infinity'])
-    #print "Number of elements after removing inapprorpirate values",len(val_list)
+    #print "Number of elements after removing inapproprirate values",len(val_list)
     
     #Convert string element to float 
     final_list=[]
@@ -83,16 +88,22 @@ def Quant(OpenName,Alpha=0.2,Num_Samples=64,Filtering=True):
         print len(filter_list)
     else:
         filter_list=final_list
+    
+    
+    #Sampling done here
+    if Sampling==True:
         
-    final=[]
-    Num_Skip=len(filter_list)/Num_Samples
-    j=0
-    k=0
-    while j < Num_Samples:
-        #print j
-        final.append(filter_list[k])
-        k+=Num_Skip
-        j+=1 
+        final=[]
+        Num_Skip=len(filter_list)/Num_Samples
+        j=0
+        k=0
+        while j < Num_Samples:
+            #print j
+            final.append(filter_list[k])
+            k+=Num_Skip
+            j+=1 
+    else:
+        final=list(filter_list)
     #print final
     #print(len(final))
     #Calculate necessary values
@@ -102,10 +113,10 @@ def Quant(OpenName,Alpha=0.2,Num_Samples=64,Filtering=True):
     Lower_Thresh=mean-(stdv*Alpha)
     
     #Printouts to let the user know about the data
-#     print "Arithmetic mean:",mean
-#     print "Stan. deviation:",stdv
-#     print "Upper Threshold:",Upper_Thresh
-#     print "Lower Threshold:",Lower_Thresh
+    print "Arithmetic mean:",mean
+    print "Stan. deviation:",stdv
+    print "Upper Threshold:",Upper_Thresh
+    print "Lower Threshold:",Lower_Thresh
     #print final
     
     #Quantization
@@ -116,15 +127,19 @@ def Quant(OpenName,Alpha=0.2,Num_Samples=64,Filtering=True):
         elif final[i] <= Lower_Thresh:
             csv_list.append(0)
     
+    #Create folder
+    if not os.path.exists(os.getcwd()+"/Output_Data/"+OpenName):
+        os.makedirs(os.getcwd()+"/Output_Data/"+OpenName)
+    
     #Output to CSV file
-    outputCSVFile = open(os.getcwd()+"//Output_Data//"+OpenName+"\\"+OpenName+"A_"+str(Alpha)+"S_"+str(Num_Samples)+".csv",'wb')
+    outputCSVFile = open(os.getcwd()+"/Output_Data/"+OpenName+"/"+OpenName+"A_"+str(Alpha)+"S_"+str(Num_Samples)+".csv",'wb')
     wr = csv.writer(outputCSVFile,quoting=csv.QUOTE_ALL)
     wr.writerow(csv_list)
     outputCSVFile.close()
     csvfile.close()
     
     #Output to ASCII file for NIST Analysis
-    outputASCIIFile = open(os.getcwd()+"//Output_Data//"+OpenName+"\\"+OpenName+"A_"+str(Alpha)+"S_"+str(Num_Samples)+".dat",'w')
+    outputASCIIFile = open(os.getcwd()+"/Output_Data/"+OpenName+"/"+OpenName+"A_"+str(Alpha)+"S_"+str(Num_Samples)+".dat",'w')
     for item in csv_list:
 #         print item
         outputASCIIFile.write(str(item))
@@ -133,25 +148,19 @@ def Quant(OpenName,Alpha=0.2,Num_Samples=64,Filtering=True):
 #     print(bin)
 #     outputBINFile.write(bin)
     outputASCIIFile.close()
+
 Name1="Readings_Alice"
 Name2="Readings_Bob"
 i=0
 Sample=64
 SSample=str(Sample)
-while i < 10:
-    Quant2(Name1, Name2, i/10.00, Sample, False)
-    compare2(Name1+"A_"+str(i/10.00)+"S_"+SSample,Name2+"A_"+str(i/10.00)+"S_"+SSample)
-    i+=1
-    
+
+
+Quant(Name1,0.3333,0,False)
+
+# while i < 10:
+#     Quant2(Name1, Name2, i/10.00, Sample, False)
+#     compare2(Name1+"A_"+str(i/10.00)+"S_"+SSample,Name2+"A_"+str(i/10.00)+"S_"+SSample)
+#     i+=1
 # Quant(Name1,0.1,64,False)
-# Quant(Name1,0.2,64,False)
-# Quant(Name1,0.3,64,False)
-# Quant(Name1,0.4,64,False)
-# Quant(Name2,0.1,64,False)
-# Quant(Name2,0.2,64,False)
-# Quant(Name2,0.3,64,False)
-# Quant(Name2,0.4,64,False)
 # compare2("Readings_AliceA_0.1S_64", "Readings_BobA_0.1S_64")
-# compare2("Readings_AliceA_0.2S_64", "Readings_BobA_0.2S_64")
-# compare2("Readings_AliceA_0.3S_64", "Readings_BobA_0.3S_64")
-# compare2("Readings_AliceA_0.4S_64", "Readings_BobA_0.4S_64")
